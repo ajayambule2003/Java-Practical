593522c68a3b754b5700124b20bc29de5cab2e39
Ajay Ambule <ajayambule2003@gmail.com>
2024-06-15 22:28:09 +0530

    Added array rotation program

---
+++ Encapsulation.java
@@ -0,0 +1,34 @@
+// Java Encapsulation
+class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setName("Ajay");
+        person.setAge(21);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
