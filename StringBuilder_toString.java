0778899174642dd6d8a68a5ef3824459813d2ffd
Ajay Ambule <ajayambule2003@gmail.com>
2024-06-27 13:16:17 +0530

    New program solved

---
+++ StringBuilder_toString.java
@@ -0,0 +1,13 @@
+
+// Java program to demonstrate toString() Method
+import java.io.*;
+
+class StringBuilder_toString {
+
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("Java Programming");
+        System.out.println("String contains = "
+                + str.toString());
+    }
+}
