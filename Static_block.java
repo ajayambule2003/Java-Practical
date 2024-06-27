5a657fadc557d2dd75eb3deb172c9c35909cf614
Ajay Ambule <ajayambule2003@gmail.com>
2024-06-27 13:12:03 +0530

    Updated Files

---
+++ Static_block.java
@@ -0,0 +1,16 @@
+//Java program to demonstrate use of static blocks
+class Test {
+
+	static int a = 10;
+	static int b;
+	static {
+		System.out.println("Static block initialized.");
+		b = a * 4;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("from main");
+		System.out.println("Value of a : " + a);
+		System.out.println("Value of b : " + b);
+	}
+}
