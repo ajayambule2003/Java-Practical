35b8bcc52d9a88fdb67e697632dcf99e635d82c9
Ajay Ambule <ajayambule2003@gmail.com>
2024-07-14 13:04:42 +0530

    new problem solution

---
+++ new1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class new1 {
+
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("/Desktop/new1.txt");
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
