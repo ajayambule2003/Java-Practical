844a6926ff18ef6881bac5c4ed6f0d6fae877d38
Ajay Ambule <ajayambule2003@gmail.com>
2024-07-16 19:06:25 +0530

    Here is new problem soution

---
+++ time.java
@@ -0,0 +1,21 @@
+// Java program to convert 24 hour  time format to 12 hour format
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class time {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println("Current Time is : " + date);
+
+        SimpleDateFormat formatTime = new SimpleDateFormat("hh.mm aa");
+
+        String time = formatTime.format(
+                date);
+
+        System.out.println(
+                "Current Time in AM/PM Format is : " + time);
+    }
+}
