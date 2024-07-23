f22ac61080dd6b63983194b9cc0228d896b2f36e
Ajay Ambule <ajayambule2003@gmail.com>
2024-07-23 21:04:30 +0530

    New program solution

---
+++ list.java
@@ -0,0 +1,22 @@
+// Java program to Insert Elements in LinkedList at first and last position to showcase addFirst() and addlast() Method 
+
+import java.util.*;
+
+public class list {
+
+    public static void main(String args[]) {
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        linkedList.add("e");
+        linkedList.add("e");
+        linkedList.add("k");
+        System.out.println("Linked list: " + linkedList);
+
+        linkedList.addFirst("G");
+
+        linkedList.addLast("s");
+
+        System.out.println("Updated Linked list: "
+                + linkedList);
+    }
+}
