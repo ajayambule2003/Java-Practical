86ed02a8d4f4b40f914918dbc4805fa4d5d4086b
Ajay Ambule <ajayambule2003@gmail.com>
2024-08-06 00:39:11 +0530

    new Problem solution

---
+++ array.java
@@ -0,0 +1,19 @@
+// Java Program to find the if the arrays are equal 
+
+import java.util.Arrays; 
+public class array { 
+	public static void main(String[] args) 
+	{ 
+		int a[] = { 30, 25, 40 }; 
+		int b[] = { 30, 25, 40 }; 
+ 
+		boolean result = Arrays.equals(a, b); 
+
+		if (result == true) {  
+			System.out.println("Two arrays are equal"); 
+		} 
+		else { 
+			System.out.println("Two arrays are not equal"); 
+		} 
+	} 
+}
