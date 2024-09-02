b42c225a66e0c4bd4a1a1efcc800a405b1dcf6f1
Ajay Ambule <ajayambule2003@gmail.com>
2024-09-03 00:16:42 +0530

    New Problem Solution

---
+++ LCM.java
@@ -0,0 +1,19 @@
+//Java Program to find the LCM of two numbers
+
+import java.io.*;
+class LCM {
+    public static void main(String[] args)
+    {
+        int a = 15, b = 25;
+        int ans = (a > b) ? a : b;
+
+        while (true) {
+            if (ans % a == 0 && ans % b == 0)
+                break;
+            ans++;
+        }
+
+        System.out.println("LCM of " + a + " and " + b
+                           + " : " + ans);
+    }
+}
