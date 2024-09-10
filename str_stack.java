b872712a0cfe2fec8247cff1b49523f64c67c253
Ajay Ambule <ajayambule2003@gmail.com>
2024-09-10 22:04:38 +0530

    New Probelm solution

---
+++ str_stack.java
@@ -0,0 +1,31 @@
+// Java Program to Reverse a String using Stack 
+import java.io.*; 
+import java.util.*; 
+class str_stack { 
+
+	public static String ReverseString(String str) 
+	{ 
+		char[] reverseString = new char[str.length()]; 
+		Stack<Character> stack = new Stack<Character>(); 
+
+		for (int i = 0; i < str.length(); i++) { 
+			stack.push(str.charAt(i)); 
+		} 
+		int i = 0; 
+		while (!stack.isEmpty()) { 
+			reverseString[i++] = stack.pop(); 
+		} 
+		return new String(reverseString); 
+	} 
+	public static void main(String[] args) 
+	{ 
+		String str1 = "Java Programming"; 
+		
+		System.out.println(str1 + " <- Reverse -> "
+						+ ReverseString(str1)); 
+		
+		String str2 = "Hello World";  
+		System.out.println(str2 + " <- Reverse -> "
+						+ ReverseString(str2)); 
+	} 
+}
