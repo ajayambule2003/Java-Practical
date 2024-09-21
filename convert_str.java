0ed83baaa51dda5775cb149f85bd45c31ee3f6fc
Ajay Ambule <ajayambule2003@gmail.com>
2024-09-21 20:35:53 +0530

    New file added

---
+++ convert_str.java
@@ -0,0 +1,21 @@
+// Java Program to convert string to byte 
+
+class convert_str {
+
+    public static byte convertStringToByte(String str) {
+        return Byte.parseByte(str);
+    }
+
+    public static void main(String[] args) {
+
+        String stringValue = "1";
+
+        byte byteValue;
+        byteValue = convertStringToByte(stringValue);
+
+        System.out.println(
+                stringValue
+                + " after converting into byte = "
+                + byteValue);
+    }
+}
