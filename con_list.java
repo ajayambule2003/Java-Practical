ae1707f4f140fcbc8d9b5ee4fda96e2df682f8e6
Ajay Ambule <ajayambule2003@gmail.com>
2024-09-24 23:14:53 +0530

    New Program File added

---
+++ con_list.java
@@ -0,0 +1,24 @@
+// Java Program to convert List to Stream in Java 8
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class con_list {
+    private static <T> Stream<T> convertListToStream(List<T> list)
+    {
+        return list.stream();
+    }
+
+    public static void main(String args[])
+    {
+        List<String> list = Arrays.asList("Java Programming",
+                                          "A computer portal", 
+                                          "for development");
+        System.out.println("List: " + list);
+
+        Stream<String> stream = convertListToStream(list);
+
+        System.out.println("Stream from List: "
+                    + Arrays.toString(stream.toArray()));
+    }
+}
