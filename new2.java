30e81d6e8576f62f390fbba2e13e0a3ee8484ecf
Ajay Ambule <ajayambule2003@gmail.com>
2025-05-24 10:34:04 +0530

    new file:

--- new2.java
+++ new2.java
@@ -11,10 +11,11 @@ public class new2 {
         b[0]++;
         System.out.println("Contents of a[] ");
 
+
         for (int i = 0; i < a.length; i++)
             System.out.print(a[i] + " ");
 
-        System.out.println("\n\nContents of b[] ");
+        System.out.println("\n\nContents of b[] "); 
 
         for (int i = 0; i < b.length; i++)
             System.out.print(b[i] + " ");
