db390ac27524f6cdf4e52142709cfdc75296e4ed
Ajay Ambule <ajayambule2003@gmail.com>
2025-06-27 22:49:50 +0530

    new file:

--- s_string.java
+++ s_string.java
@@ -12,6 +12,6 @@ class s_string {
 
 		System.out.println(
 				"Type of str : "
-	 					+ ((Object) str).getClass().getSimpleName());
-	 }
- }
+      	 		 			+ ((Object) str).getClass().getSimpleName());
+	}
+}
