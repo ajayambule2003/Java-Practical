6c6cd604cf74677d35022530bc2e91b2e1274840
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-07-05 17:15:07 +0530

    Update CollectionDemo.java

--- CollectionDemo.java
+++ CollectionDemo.java
@@ -7,7 +7,7 @@ class CollectionDemo {
 
 	public static void main(String[] args) {
 
-		int arr[] = new int[] { 1, 2, 3, 4 };
+		int arr[] = new int[] { 1, 2, 3, 4 }; 
 		Vector<Integer> v = new Vector();
 		Hashtable<Integer, String> h = new Hashtable();  
 		v.addElement(1);
