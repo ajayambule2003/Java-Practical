0f9a13374ce6d4067ae431500a43e5e85a4a4e3e
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-08-20 14:06:28 +0530

    Update StringBuilder_toString.java

--- StringBuilder_toString.java
+++ StringBuilder_toString.java
@@ -7,7 +7,7 @@ class StringBuilder_toString {
     public static void main(String[] args) { 
 
         StringBuilder str = new StringBuilder("Java Programming");
-        System.out.println("String contains = " 
+        System.out.println("String contains = "  
                 + str.toString());
     }
 }
