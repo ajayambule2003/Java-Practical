578af998ff6f703b5e633965d1a99e3f828832d7
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-09-13 11:37:44 +0530

    Update s_string.java

--- s_string.java
+++ s_string.java
@@ -4,7 +4,7 @@ class s_string {
 	public static void main(String[] args) {
 		double num = 12.345;
 		String str = num + "";
-		System.out.println(str);
+		System.out.println(str); 
 
 		System.out.println(
 				"Type of num : "
