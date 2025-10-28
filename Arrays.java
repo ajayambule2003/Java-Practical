415df614e099075f134945031b09639ac98974b4
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-10-28 09:01:32 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.List;
 
-class Arrays { 
+class Arrays {  
     static void Rotate(int arr[], int d, int n) { 
  
         int temp[] = new int[n];
@@ -54,5 +54,6 @@ class Arrays {
         throw new UnsupportedOperationException("Unimplemented method 'asList'");
     }
 }
+
 
 
