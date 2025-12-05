8008c5a2c3ba477bf6a571422262a4bb80740969
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-12-05 08:27:45 +0530

    Update LCM.java
    added

--- LCM.java
+++ LCM.java
@@ -5,7 +5,7 @@ class LCM {
     public static void main(String[] args)
     {
         int a = 15, b = 25;
-        int ans = (a > b) ? a : b;
+        int ans = (a > b) ? a : b; 
 
         while (true) {
             if (ans % a == 0 && ans % b == 0)
