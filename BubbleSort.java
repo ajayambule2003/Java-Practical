5be89c675fd5e8322ea4b3413767a3f5bce67e78
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-12-17 19:16:34 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,7 +6,7 @@ class BubbleSort {
         int n = arr.length; 
         for (int i = 0; i < n - 1; i++) { 
             for (int j = 0; j < n - i - 1; j++) { 
-                if (arr[j] > arr[j + 1]) { 
+                if (arr[j] > arr[j + 1]) {  
  
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
